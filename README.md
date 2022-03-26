49a8a8427b831e4cd0e49a8c4c4e518ab2116d70
Rochak <rochak.bhardwaj.ece19@itbhu.ac.in>
2022-03-26 21:41:17 +0530

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HeyDoctor, A Smart Telemedicine WebApp
+This web-app is an initiative to digitalize the medical field and make appointments between doctors and patients more interactive.![homepage](https://user-images.githubusercontent.com/56127516/130402485-4a398978-d69b-4687-9150-4d1a911bcfa2.png)
+
---
+++ server/routes/patientRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/patientAuth');
+const patientController = require('../controller/patientController');
+
+router.get("/profile", auth, patientController.getProfile);
+router.post("/profile",auth, patientController.saveProfile);
+router.get("/getDoctors",auth, patientController.getDoctors);
+router.post("/sendRequest/:id",auth,patientController.sendRequest);
+router.post("/cancelRequest/:id",auth,patientController.cancelRequest);
+router.get("/getAppointedDoctors",auth,patientController.getAppointedDoctors);
+router.post("/sendProblem/:id",auth,patientController.sendProblem);
+
+module.exports = router;
